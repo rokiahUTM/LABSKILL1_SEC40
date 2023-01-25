72b4421d6ff21235bb56c9b67a478744fedd9c58
rokiahUTM <53244657+rokiahUTM@users.noreply.github.com>
2023-01-25 11:58:08 +0800

    25 Jan 2023

--- app/src/main/java/com/example/labskill1/LoginActivity.java
+++ app/src/main/java/com/example/labskill1/LoginActivity.java
@@ -2,13 +2,62 @@ package com.example.labskill1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton,cancelButton;
+    EditText usernameET,passwordET;
+
+    TextView tx1;
+    int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button)findViewById(R.id.loginBtn);
+        usernameET = (EditText)findViewById(R.id.usernameET);
+        passwordET = (EditText)findViewById(R.id.passwordET);
+
+        cancelButton = (Button)findViewById(R.id.cancelLoginBtn);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(usernameET.getText().toString().equals("admin") && passwordET.getText().toString().equals("admin")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        loginButton.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
